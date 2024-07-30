fa9e2bf45d585b4f465aa7fef784f6646b0a00f9
sparrowsl <benthorpe.dev@gmail.com>
2024-07-30 12:52:41 +0000

    update setup script

--- dotfiles/setup.sh
+++ dotfiles/setup.sh
@@ -3,7 +3,7 @@
 
 # Needed for all installers (update system)
 sudo apt update -y
-sudo apt install -y curl unzip xclip mpv wget tar fzf ripgrep btop fd flameshop peek evolution gthumb guake
+sudo apt install -y curl unzip xclip mpv wget tar fzf ripgrep btop fd peek geary gthumb guake
 
 
 
@@ -23,9 +23,32 @@ sudo n lts
 # Remove old node versions
 sudo n prune
 
+# Install Bun
+curl -fsSL https://bun.sh/install | bash
+
+# Upgrade Bun
+bun upgrade
+
+# Install PDM (Python Dependencies Manager)
+curl -sSL https://pdm-project.org/install-pdm.py | python3 -
+
+# Install Github CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+# Upgrade GH CLI
+sudo apt update
+sudo apt install gh
+
 
 
 # Download latest neovim and setup
+cd /tmp/
 wget -c github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
 tar -xvzf nvim-linux64.tar.gz
 # Rename directory
@@ -34,6 +57,7 @@ mv nvim-linux64.tar.gz nvim/
 sudo rm -rf /usr/bin/nvim/
 # Move current nvim to bin
 sudo mv ./nvim /usr/bin/
+cd -
 
 # Install VSCode
 cd ~/Downloads
@@ -42,6 +66,8 @@ sudo apt install -y ./code.deb
 rm code.deb
 cd -
 
+# Install Zed editor
+curl -f https://zed.dev/install.sh | sh
 
 # Install Zellij
 cd ~/Downloads
@@ -50,3 +76,22 @@ tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin/
 rm zellij.tar.gz
 cd -
+
+# Install Typora (Markdown Editor)
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys BA300B7755AFCFAE
+wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gpg.d/typora.asc
+# add Typora's repository
+sudo add-apt-repository 'deb https://typora.io/linux ./'
+sudo apt update
+# install typora
+sudo apt install typora
+
+# Install Brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y
+sudo apt install -y brave-browser
+
+
+
+
