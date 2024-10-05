4af29b218237199423ee81a097587855df1f9984
sparrowsl <benthorpe.dev@gmail.com>
2024-10-05 08:09:18 +0000

    chore: switch to rose-pine-moon color scheme for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,12 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 wezterm.on("gui-startup", function(cmd)
-	local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+	local _, _, window = wezterm.mux.spawn_window(cmd or {})
 	window:gui_window():maximize()
 end)
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "rose-pine-moon"
 
 config.enable_tab_bar = false
 
@@ -20,7 +20,7 @@ config.hide_tab_bar_if_only_one_tab = true
 
 -- config.window_background_image = '/path/to/wallpaper.jpg'
 
--- config.window_background_opacity = 0.95
+config.window_background_opacity = 0.99
 
 config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
